d65cb1447e05dcd39b08f29b0e7ea5dc650d2bd2
student <student@vm2-39.vua.cloud>
2021-05-20 10:55:23 +0200

    ~\4.4: Using your class

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,6 +3,7 @@
 # Exercise 3.3
 
 import csv
+import os
 import sys
 
 from colors import *
@@ -14,44 +15,48 @@ def parse_csv(filename: str, select: list = None, types: list = None,
     '''
     if select and not has_headers:
         raise RuntimeError("select argument requires column headers")
-
-    with open(filename) as f:
-        rows = csv.reader(f, delimiter=delimiter)
-        if has_headers:
-            headers = next(rows)
-        else:
-            headers = []
-        if select is None:
-            select = ['name', 'shares', 'price']
-            indices = []
-        else:
-            indices = [headers.index(colname) for colname in select]
-            headers = select
-        records = []
-        for i, row in enumerate(rows, 1):
-            if not row:
+    isFilenameAFile = False
+    try:
+        isFilenameAFile = os.path.isfile(filename)
+        print(f'Parameter %s is a file.' % filename)
+    except Exception:
+        print("Parameter is not a file.")
+    if isFilenameAFile:
+        filename = open(filename)
+    rows = csv.reader(filename, delimiter=delimiter)
+    if has_headers:
+        headers = next(rows)
+    else:
+        headers = []
+    if select is None:
+        select = ['name', 'shares', 'price']
+        indices = []
+    else:
+        indices = [headers.index(colname) for colname in select]
+        headers = select
+    records = []
+    for i, row in enumerate(rows, 1):
+        if not row:
+            continue
+        if indices:
+            row = [row[index] for index in indices]
+        if types:
+            try:
+                row = [func(val) for func, val in zip(types, row)]
+            except Exception as e:
+                if not silence_errors:
+                    sys.stdout.write(RED)
+                    print(f'File "{filename}" | Row {i}: Couldn\'t convert {row}')
+                    print('Row %d: Reason %s' % (i, e))
+                    sys.stdout.write(RESET)
+                # raise
                 continue
-            if indices:
-                row = [row[index] for index in indices]
-            if types:
-                try:
-                    row = [func(val) for func, val in zip(types, row)]
-                except Exception as e:
-                    if not silence_errors:
-                        sys.stdout.write(RED)
-                        print(f'File "{filename}" | Row {i}: Couldn\'t convert {row}')
-                        print('Row %d: Reason %s' % (i, e))
-                        sys.stdout.write(RESET)
-                    #raise
-                    continue
 
-            if has_headers:
-                record = dict(zip(headers, row))
-            else:
-                record = tuple(row)
-            records.append(record)
+        if has_headers:
+            record = dict(zip(headers, row))
+        else:
+            record = tuple(row)
+        records.append(record)
+    if isFilenameAFile:
+        filename.close()
     return records
-
-
-#dictio = parse_csv('Data/missing.csv', types=[str, int, float])
-#print(dictio)
